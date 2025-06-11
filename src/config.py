fdb56d73d93912a637ae4da30ff49630e6bcba7b
FeiPower <38192118+FeiPower@users.noreply.github.com>
2025-06-11 12:42:07 -0600

    update

--- src/config.py
+++ src/config.py
@@ -8,4 +8,4 @@ DATA_DIR = BASE_DIR / "src" / "data"
 GIS_DATA_DIR = DATA_DIR / "gis"
 
 # GeoJSON files
-OBRERA_GEOJSON = GIS_DATA_DIR / "002_Obrera.geojson" 
+# OBRERA_GEOJSON = GIS_DATA_DIR / "002_Obrera.geojson"  # File removed - no longer used 
